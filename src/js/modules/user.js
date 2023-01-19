25f4dd1b7238b2c6160fff3a1d2e9d4e26fd558f
Dmitrii Mv <hotrhamster@gmail.com>
2023-01-19 11:15:44 +0300

    prepared for module restructuring, changed rendering to rects

---
+++ src/js/modules/user.js
@@ -0,0 +1,13 @@
+class User {
+    constructor(name) {
+      this.name = name;
+    }
+    static new(props) {
+      return new this(props.name);
+    }
+    greet() {
+      return `Hello, I'm ${this.name}!`;
+    }
+  }
+
+  export default User;
