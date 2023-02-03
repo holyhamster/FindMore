a17f5e5f1e3d28a6707518b9ebb80090fe7a88db
Dmitrii Mv <hotrhamster@gmail.com>
2023-02-03 13:43:17 +0300

    Revert "Revert "separated searchregion into separate class""
    This reverts commit da1c298388566ddf182cea8669282a250b75a454.

--- src/js/domSearcher.js
+++ src/js/domSearcher.js
@@ -39,7 +39,8 @@ class DomSearcher{
   startSearch()
   {
     //this.showNodes()
-    this.searchRecursive(this.searchString, DomSearcher.initRegion(), [], new Map(), this.getWalk(), this.regexString);
+    const region = new SearchRegion(this.getWalk(), this.searchString, this.regexString);
+    this.searchRecursive(region, [], new Map());
   }
   //iterates through a search region, continues pulling new nodes out of this.walk when search fails
   //makes a timeout everytime it makes too many calls at once (to give the browser some rendering time)
@@ -54,7 +55,7 @@ class DomSearcher{
       console.log(node);
     }
   }
-  searchRecursive(_searchString, _region, _matches, _highlightGroups, _walk, _regexp)
+  searchRecursive(_searchRegion, _matches, _highlightGroups)
   {
     let callsLeft = this.consecutiveCalls;
     let range = document.createRange();
@@ -66,28 +67,28 @@ class DomSearcher{
       if (_matches.length > 0)
       {
         let match = _matches.shift();
-        let newHL = DomSearcher.processMatch(match, _region, range, _highlightGroups);
+        let newHL = DomSearcher.processMatch(match, _searchRegion, range, _highlightGroups);
         if (newHL && !dirtyHLGroups.get(newHL))
           dirtyHLGroups.set(newHL.parent, newHL.group);
 
         if (_matches.length == 0)
-          DomSearcher.trimRegionToPoint(_region, match.endIndex, match.endOffset);
+          _searchRegion.trimToPoint(match.endIndex, match.endOffset);
       }
       else if (callsLeft > 0)
       {
 
-        _matches = DomSearcher.getAllMatches(callsLeft, _region, _searchString, _regexp);
-        if (_matches.length == 0)  //try basing this on offset
+        _matches = _searchRegion.getMatches(callsLeft);
+        if (_matches.length == 0)  //try basing this on offset <<--???
         {
-          let WALK_IN_PROGRESS = DomSearcher.expandRegion(_region, _walk);
-          DomSearcher.trimRegion(_region, _searchString);
+          let WALK_IN_PROGRESS = _searchRegion.addNextNode();
+
           if (!WALK_IN_PROGRESS)
           {
             dirtyHLGroups.forEach(function(_hlGroup) {_hlGroup.commit(); });
             console.log("end of tree");
             return;
           }
-          _matches = DomSearcher.getAllMatches(callsLeft, _region, _searchString, _regexp);
+          _matches = _searchRegion.getMatches(callsLeft);
         }
       }
     }
@@ -101,7 +102,7 @@ class DomSearcher{
     dirtyHLGroups.forEach(function(_hlGroup) {_hlGroup.commit(); });
 
     setTimeout( function() { this.searchRecursive.call(
-      this, _searchString, _region, _matches, _highlightGroups, _walk, _regexp) }.bind(this), this.interval);
+      this, _searchRegion, _matches, _highlightGroups) }.bind(this), this.interval);
   }
 
   static initRegion()
