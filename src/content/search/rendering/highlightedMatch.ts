7a95cb48917a94939c2a97d5af75874acd8dd6cb
Dmitrii Mv <hotrhamster@gmail.com>
2023-04-14 14:26:19 +0300

    renamed and made an extension of IndexedMatch

---
+++ src/content/search/rendering/highlightedMatch.ts
@@ -0,0 +1,44 @@
+import { IndexedMatch } from "./indexedMatch";
+
+//match with
+export class HighlightedMatch extends IndexedMatch {
+    private elements: Element[] = [];
+    constructor(match: IndexedMatch, searchId: number, rects: DOMRect[], anchor: DOMRect) {
+        super(match, match.index);
+        rects.forEach((rect) =>
+            this.elements.push(createElement(match.index, searchId, rect, anchor)));
+    }
+
+    public AppendSelf(parent: Element) {
+        this.elements.forEach((element: Element) =>
+            parent.append(element));
+    }
+
+    public SetAccent(state: boolean) {
+        this.elements.forEach((span: Element) => {
+            if (state)
+                span.classList.add(`fm-accented`);
+            else
+                span.classList.remove(`fm-accented`);
+        });
+    }
+}
+
+//create a custom html element with id classes
+function createElement(matchId: number, searchId: number,
+    rect: DOMRect, anchor: DOMRect): HTMLElement {
+    const element = document.createElement('FM-HIGHLIGHT');
+    element.classList.add(`fm-${searchId}`, `fm-${searchId}-${matchId}`);
+    Object.assign(element.style, rectangleToStyle(rect, anchor))
+    return element
+}
+
+//describe rectangle relative to given anchor as a .style object
+function rectangleToStyle(rect: DOMRect, anchor: DOMRect) {
+    return {
+        height: rect.height + 'px',
+        width: rect.width + 'px',
+        left: rect.left - anchor.x + 'px',
+        top: rect.top - anchor.y + 'px'
+    }
+}
